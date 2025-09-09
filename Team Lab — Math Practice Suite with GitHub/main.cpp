b4ccaf490b982bc02fc9c1acef18f3f9c9ec828b
Yidi428 <116138589+Yidi428@users.noreply.github.com>
2025-09-09 20:15:42 +0000

    Implement basic menu for Math Practice Suite with options for Addition, Subtraction, Multiplication, Division, and Statistics

--- Team Lab — Math Practice Suite with GitHub/main.cpp
+++ Team Lab — Math Practice Suite with GitHub/main.cpp
@@ -2,5 +2,15 @@
 using namespace std;
 
 int main(){
+    int answer;
+    int choice;
+
+
+    //Show a menu with options for Addition, Subtraction, Multiplication, Division, and Statistics.
+
+    cout << "Hello, welcome to Math Practice! Enter a number for choosing subject." << endl;
+    cout << endl << "1. Addition" << endl << "2. Subtractions" << endl << "3. Multiplication" << endl << "4. Division" << endl << "5. Statistics" << endl << endl << "Enter number: ";
+    cin >> choice;
+    
     return 0;
 }
