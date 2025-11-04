3323b93f7f06364b489c85f4f77f646c60c629ae
Yidi428 <116138589+Yidi428@users.noreply.github.com>
2025-11-04 20:31:58 +0000

    Saving progress for Most Frequent Character

--- Lab 10/Most_Frequent_Character/main.cpp
+++ Lab 10/Most_Frequent_Character/main.cpp
@@ -3,9 +3,9 @@
 #include <string>
 using namespace std;
 
-string mostFrequentCharacter() {
+/* string mostFrequentCharacter() {
     return;
-}
+} */
 
 int main() {
     
@@ -14,7 +14,22 @@ int main() {
     cout << "Please enter one word: ";
     cin >> str;
 
+    char arr[str.length()];
+
+    strcpy(arr, str.c_str());
+
     cout << "In your word, here's the most repeating characters: ";
+
+    //prints char array
+    /* for (int i = 0; i < strlen(arr); i++) {
+        cout << arr[i] << " " ;
+    } */
+
+    //cout << endl
+
+    for (int i = 0; i < sizeof(arr); i++) {
+
+    }
 
     return 0;
 }
