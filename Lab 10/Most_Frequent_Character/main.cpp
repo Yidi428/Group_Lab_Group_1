0ac53defccb62511e38ac8ae664972050ef4f414
Yidi428 <116138589+Yidi428@users.noreply.github.com>
2025-11-04 19:45:56 +0000

    Completing lab 10.

--- Lab 10/Most_Frequent_Character/main.cpp
+++ Lab 10/Most_Frequent_Character/main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+#include <cstring>
+#include <string>
 using namespace std;
 
+void mostFrequentCharacter() {
+
+}
+
 int main() {
+
+    int size = 0;
+
+    string str;
+
+    char array[size];
+
+    cout << "Enter only one word: ";
+    cin >> str;
+
+    cout << endl;
+
+    cout << "Here's the string: " << str;
+
     return 0;
 }
