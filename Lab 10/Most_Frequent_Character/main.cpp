7f2ef06256771a0b49e61784fe1db2804ad4e8f1
Yidi428 <huskyakadharma@gmail.com>
2025-11-11 19:55:10 -0800

    Saving progress. Completed two labs.

--- Lab 10/Most_Frequent_Character/main.cpp
+++ Lab 10/Most_Frequent_Character/main.cpp
@@ -1,47 +1,51 @@
 #include <iostream>
 #include <cstring>
 #include <string>
+#include <vector>
 using namespace std;
 
- char* mostFrequentCharacter(char* arr, int size) {
-    char* newArr = new char[size];
-
-    for (int i = 0; i < size; i++) {
-        
+void mostFrequentCharacter(char* cstr) {
+    vector<char> charCollection;
+    vector<int> charOccurrence;
+    for (int i = 0; i < strlen(cstr); i++) {
+        char current = cstr[i];
+        bool found = false;
+        for (int j = 0; j < charCollection.size(); j++) {
+            if (charCollection[j] == current) {
+                charOccurrence[j]++;
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            charCollection.push_back(current);
+            charOccurrence.push_back(1);
+        }
     }
 
-    return "";
+    int max = 0;
+    for (int i = 1; i < charOccurrence.size(); i++) {
+        if (charOccurrence[i] > charOccurrence[max]) {
+            max = i;
+        }
+    }
+    cout << "Most frequent character: " << charCollection[max] << endl;
 }
 
 int main() {
-    
     string str;
-
-    cout << "Please enter one word: ";
-
+    
+    cout << "Enter a word that's less than 100 characters (no spaces. Ex. 'Banana' not 'Banana Banana'): ";
+    
     cin >> str;
-
-    char arr[str.length()];
-
-    int arraySize = sizeof(arr);
-
-    strcpy(arr, str.c_str());
-
-    cout << "In your word, here's the most repeating characters: ";
-
-    //prints char array
-
-    /* for (int i = 0; i < strlen(arr); i++) {
-
-        cout << arr[i] << " " ;
-
-    } */
-
-    //cout << endl
-
-    //come back later
-
-    mostFrequentCharacter(arr, arraySize);
-
+    
+    int size = str.length();
+    
+    char word[size];
+    
+    strcpy(word, str.c_str());
+    
+    mostFrequentCharacter(word);
+    
     return 0;
 }
--- Lab 10/Password_Verifier/main.cpp
+++ Lab 10/Password_Verifier/main.cpp
@@ -1,8 +1,60 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-
-
 int main() {
+
+    string pass;
+    bool oneUpperCase = false;
+    bool hasDigit = false;
+    bool isSixDigits = false;
+    bool oneLowerCase = false;
+
+    cout << "Enter your password (no spaces): ";
+
+    cin >> pass;
+
+    char passArray[pass.size() + 1];
+    strcpy(passArray, pass.c_str());
+
+
+    if ( strlen(passArray) >= 6 ) {
+
+        isSixDigits = true;
+        
+    }
+
+    for ( int i = 0; i < strlen(passArray); ++i ) {
+
+        if ( isupper(passArray[i]) ) {
+            oneUpperCase = true;
+        }
+
+        if ( islower(passArray[i]) ) {
+            oneLowerCase = true;
+        }
+        
+    }
+
+    for ( int i = 0; i < strlen(passArray); ++i ) {
+
+        if ( isdigit(passArray[i]) ) {
+            hasDigit = true;
+        }
+        
+    }
+
+    if ( oneLowerCase && isSixDigits && hasDigit && oneUpperCase ) {
+
+        cout << "Good to go! Use the password!." << endl;
+        
+    } else {
+
+        cout << "Password did not meet criteria." << endl;
+        cout << "Criteria:" << endl;
+        cout << "At least one lowercase letter, one uppercase letter, one digit, and at least 6 characters." << endl;
+        
+    }
+
     return 0;
 }
